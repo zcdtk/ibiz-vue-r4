4d372839859b631eb6820959d7592bb87cd1eda4
zcdtk <zcdtk@github.com>
2019-03-14 11:17:25 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,14 @@ import store from './store';
 
 Vue.config.productionTip = false;
 
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(iView);
+Vue.use(ElementUI);
+
 new Vue({
   router,
   store,
